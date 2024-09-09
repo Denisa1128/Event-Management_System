a3bc062a5ca050982bf2fccb10e03f66e58a3e29
Denisa Ciocani <denisaciocani1128@gmail.com>
2024-09-09 15:16:20 +0300

    User Repository

---
+++ src/main/java/com/itschool/eventmanagment/models/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.itschool.eventmanagment.models.repositories;
+
+
+import com.itschool.eventmanagment.models.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
