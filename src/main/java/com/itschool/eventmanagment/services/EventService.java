a80b4db98e8488452d752de8154aff97b484e6f1
alinaberlin <alina.ghetler@gmail.com>
2024-09-12 22:40:03 +0200

    Add empty service events seervices and services

---
+++ src/main/java/com/itschool/eventmanagment/services/EventService.java
@@ -0,0 +1,14 @@
+package com.itschool.eventmanagment.services;
+
+import com.itschool.eventmanagment.models.dtos.EventDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface EventService {
+    EventDTO createEvent(EventDTO eventDTO);
+    List<EventDTO> deleteEvent();
+    List<EventDTO> updateEvent();
+    List<EventDTO> getEvents();
+}
---
+++ src/main/java/com/itschool/eventmanagment/services/EventServiceImp.java
@@ -0,0 +1,27 @@
+package com.itschool.eventmanagment.services;
+
+import com.itschool.eventmanagment.models.dtos.EventDTO;
+
+import java.util.List;
+
+public class EventServiceImp implements EventService {
+    @Override
+    public EventDTO createEvent(EventDTO eventDTO) {
+        return null;
+    }
+
+    @Override
+    public List<EventDTO> deleteEvent() {
+        return null;
+    }
+
+    @Override
+    public List<EventDTO> updateEvent() {
+        return null;
+    }
+
+    @Override
+    public List<EventDTO> getEvents() {
+        return null;
+    }
+}
