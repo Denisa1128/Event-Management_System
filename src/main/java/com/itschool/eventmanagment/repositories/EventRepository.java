28d56b3e739840bf38c56cb5f1a51c19a50abc08
alinaberlin <alina.ghetler@gmail.com>
2024-09-12 23:21:14 +0200

    Add event repository

--- src/main/java/com/itschool/eventmanagment/repositories/EventRepository.java
+++ src/main/java/com/itschool/eventmanagment/repositories/EventRepository.java
@@ -2,6 +2,8 @@ package com.itschool.eventmanagment.repositories;
 
 import com.itschool.eventmanagment.models.entities.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EventRepository extends JpaRepository<Event, Long > {
 }
