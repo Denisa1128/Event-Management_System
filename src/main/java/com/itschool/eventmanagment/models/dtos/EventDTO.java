e6bc91f1f2ff39aa9134eb5cda744cfc693495d7
alinaberlin <alina.ghetler@gmail.com>
2024-09-13 11:57:11 +0200

    Create a POST endpoint for events

--- src/main/java/com/itschool/eventmanagment/models/dtos/EventDTO.java
+++ src/main/java/com/itschool/eventmanagment/models/dtos/EventDTO.java
@@ -1,5 +1,6 @@
 package com.itschool.eventmanagment.models.dtos;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.itschool.eventmanagment.models.entities.Participant;
 import lombok.Data;
 
@@ -9,11 +10,11 @@ import java.util.List;
 
 @Data
 public class EventDTO {
-    private Long Id;
+    private Long id;
     private String name;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime dateTime;
     private String location;
-
     private int maxParticipants;
     private List<Participant> registeredParticipants = new ArrayList<>();
 }
--- src/main/java/com/itschool/eventmanagment/services/EventService.java
+++ src/main/java/com/itschool/eventmanagment/services/EventService.java
@@ -4,8 +4,6 @@ import com.itschool.eventmanagment.models.dtos.EventDTO;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-
-@Service
 public interface EventService {
     EventDTO createEvent(EventDTO eventDTO);
     List<EventDTO> deleteEvent();
